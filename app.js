d87ea475da586df6e94a0d6bd7576598a3561272
Prathana <prathanay40@gmail.com>
2025-11-23 17:43:48 +0530

    Refactor: Reorganize tours API into MVC pattern

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 const express = require("express");
 const app = express();
 
-const {
-  getAllTours,
-  getTourById,
-  createTour,
-  updateTour,
-  deleteTour,
-} = require("./tourHandlers.js"); 
+ 
+// Import the router (instead of individual functions)
+const tourRouter = require('./routes/tourRouter.js');
 
 // Middleware to parse JSON
 app.use(express.json());
 
-// ROUTES
-// GET /tours
-app.get("/tours", getAllTours);
-
-// POST /tours
-app.post("/tours", createTour);
-
-// GET /tours/:tourId
-app.get("/tours/:tourId", getTourById);
-
-// PUT /tours/:tourId
-app.put("/tours/:tourId", updateTour);
-
-// DELETE /tours/:tourId
-app.delete("/tours/:tourId", deleteTour);
+ app.use('/tours', tourRouter);
 
 const port = 4000;
 // Start the server
